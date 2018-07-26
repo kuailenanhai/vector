4cc456645dfef6ef97a1c5284665c0173de5ac9c
kuailenanhai <2651472973@qq.com>
2018-07-26 09:10:25 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<vector>
+int main()
+{
+	std::cout<<"hello ,today is a new day,"
+			   "wonderful!!"
+			   "hello"<<std::endl;
+	system("pause");
+	return 0;
+}
